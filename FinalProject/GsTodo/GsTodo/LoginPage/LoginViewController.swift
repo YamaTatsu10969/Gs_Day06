4ad1f87dd01b1aa3b95e9e093e133adb2c14d896
yamamoto tatsuya <52.wadachi.96@gmail.com>
2020-01-27 21:59:57 +0900

    add login up

---
+++ FinalProject/GsTodo/GsTodo/LoginPage/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  GsTodo
+//
+//  Created by Tatsuya Yamamoto on 2020/1/27.
+//  Copyright © 2020 yamamototatsuya. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tapSignUpButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapLogInButton(_ sender: Any) {
+    }
+    
+    @IBAction func tapTermsButton(_ sender: Any) {
+    }
+    
+}
